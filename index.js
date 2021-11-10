d3acaf6a8a2b8b997d083c54e422f10a8ad693f1
Akash Verma <65110494+shawnfrost69@users.noreply.github.com>
2021-11-11 02:35:07 +0530

    Updated Code

--- index.js
+++ index.js
@@ -254,14 +254,11 @@ addEventListener('click', (event) => {
         event.clientX - canvas.width / 2
     )
     const velocity = {
-        x: Math.cos(angle) * 5,
-        y: Math.sin(angle) * 5
+        x: Math.cos(angle) * 4,
+        y: Math.sin(angle) * 4
 
     }
-   projectiles.push(
-       new Projectile(
-       canvas.width /2, canvas.height /2, 5,'white',velocity)
-   )
+   projectiles.push(new Projectile(canvas.width /2, canvas.height /2, 5,'white',velocity))
 })
 
 startGameBtn.addEventListener('click', () =>{
